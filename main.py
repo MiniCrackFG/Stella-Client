161d3c347133afe35e3aa2f48d0a8030cc6dd104
miniloopp <rosalindareales400@gmai.com>
2026-05-10 00:23:26 -0500

    add functionality to launch Minecraft with version management and background threading

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import subprocess
+import minecraft_launcher_lib
+import os
+import threading
 import customtkinter as ctk
+
 # modo oscuro tipo launcher gaming
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -28,5 +33,43 @@ play_button = ctk.CTkButton(
     command=play
 )
 play_button.pack(pady=20)
+
+# lanzar minecraft
+def launch_minecraft():
+
+    minecraft_directory = os.path.expanduser("~/.stellaclient")
+    version = "1.21.1"   # ← usa una versión válida
+
+    # descarga si no existe
+    minecraft_launcher_lib.install.install_minecraft_version(
+        version,
+        minecraft_directory
+    )
+    options = {
+    "username": "mini",
+    "uuid": "00000000000000000000000000000000",
+    "token": "offline",
+    "executablePath": "java"
+}
+
+    command = minecraft_launcher_lib.command.get_minecraft_command(
+        version,
+        minecraft_directory,
+        options
+    )
+
+    print("Command:", command)
+    subprocess.run(command)
+
+
+# boton play
+def play():
+    print("Launching Minecraft...")
+
+    # ejecuta en segundo plano
+    threading.Thread(
+        target=launch_minecraft,
+        daemon=True
+    ).start()
 
 app.mainloop()
