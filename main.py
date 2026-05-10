5d4f9d90e1148450c60915cdd08ef917116d7457
miniloopp <rosalindareales400@gmai.com>
2026-05-10 08:42:08 -0500

    refactor main.py for improved Minecraft launch functionality and add log files

--- main.py
+++ main.py
@@ -4,26 +4,59 @@ import os
 import threading
 import customtkinter as ctk
 
-# modo oscuro tipo launcher gaming
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-# ventana principal
 app = ctk.CTk()
 app.title("Stella Client")
 app.geometry("900x500")
 
-# titulo
-title = ctk.CTkLabel(
-    app,
-    text="Stella Client",
-    font=("Arial", 32)
-)
+title = ctk.CTkLabel(app, text="Stella Client", font=("Arial", 32))
 title.pack(pady=40)
 
-# boton play
+
+# ---------- LAUNCH ----------
+def launch_minecraft():
+    try:
+        print("Installing / Launching...")
+
+        minecraft_directory = os.path.expanduser("~/.stellaclient")
+        version = "1.21.11"  
+
+        minecraft_launcher_lib.install.install_minecraft_version(
+            version,
+            minecraft_directory
+        )
+
+        options = {
+            "username": "mini",
+            "uuid": "00000000000000000000000000000000",
+            "token": "offline",
+            "executablePath": "java"
+        }
+
+        command = minecraft_launcher_lib.command.get_minecraft_command(
+            version,
+            minecraft_directory,
+            options
+        )
+
+        print("Starting Minecraft...")
+        subprocess.Popen(command)
+
+    except Exception as e:
+        print("ERROR:", e)
+
+
+# ---------- BUTTON ----------
 def play():
     print("Launching Minecraft...")
+
+    threading.Thread(
+        target=launch_minecraft,
+        daemon=True
+    ).start()
+
 
 play_button = ctk.CTkButton(
     app,
@@ -33,43 +66,5 @@ play_button = ctk.CTkButton(
     command=play
 )
 play_button.pack(pady=20)
-
-# lanzar minecraft
-def launch_minecraft():
-
-    minecraft_directory = os.path.expanduser("~/.stellaclient")
-    version = "1.21.1"   # ← usa una versión válida
-
-    # descarga si no existe
-    minecraft_launcher_lib.install.install_minecraft_version(
-        version,
-        minecraft_directory
-    )
-    options = {
-    "username": "mini",
-    "uuid": "00000000000000000000000000000000",
-    "token": "offline",
-    "executablePath": "java"
-}
-
-    command = minecraft_launcher_lib.command.get_minecraft_command(
-        version,
-        minecraft_directory,
-        options
-    )
-
-    print("Command:", command)
-    subprocess.run(command)
-
-
-# boton play
-def play():
-    print("Launching Minecraft...")
-
-    # ejecuta en segundo plano
-    threading.Thread(
-        target=launch_minecraft,
-        daemon=True
-    ).start()
 
 app.mainloop()
