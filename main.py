ebd817a2d0c60975d52ec96ab45801556bb0d0f5
FunKha2 <edgarkhaledgonzalez@gmail.com>
2026-05-09 19:42:36 -0600

    Sidebar, Topbar, Account and Settings

--- main.py
+++ main.py
@@ -8,6 +8,41 @@ app = ctk.CTk()
 app.title("Stella Client")
 app.geometry("900x500")
 
+# topbar
+topbar = ctk.CTkFrame(
+    app,
+    height=50,
+    fg_color="#111111"
+)
+
+topbar.pack(fill="x")
+
+# account button
+account = ctk.CTkLabel(
+    topbar,
+    text="Account",
+    font=("Arial", 16)
+)
+account.pack(side="right", padx=20, pady=10)
+
+# sidebar
+sidebar = ctk.CTkFrame(
+    app,
+    width=220,
+    fg_color="#111111"
+)
+sidebar.pack(side="left", fill="y")
+
+# Settings button
+settings = ctk.CTkLabel(
+    sidebar,
+    text="Settings",
+    font=("Arial", 16)
+)
+settings.pack(side="bottom", anchor="w", padx=20, pady=20)
+
+sidebar.pack(side="left", fill="y")
+
 # titulo
 title = ctk.CTkLabel(
     app,
@@ -29,4 +64,4 @@ play_button = ctk.CTkButton(
 )
 play_button.pack(pady=20)
 
-app.mainloop("hola")
+app.mainloop()
